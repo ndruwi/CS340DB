9539d3a9c33f265257c51fb349cdc7584afb7e71
Andrew Adisoemarta <adisoema@oregonstate.edu>
2024-03-08 07:26:10 -0800

    CRD for customers,employees,products,suppliers

---
+++ app.js
@@ -0,0 +1,289 @@
+/*
+    SETUP
+*/
+
+// Express
+var express = require('express');
+var app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+PORT = 9614;
+
+// Database
+var db = require('./database/db-connector')
+
+/*
+    ROUTES
+*/
+// Handlebars
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+/*
+    ROUTES
+*/
+
+
+app.get('/customers', function(req, res)
+{
+    let query1 = `SELECT * FROM Customers;`
+    db.pool.query(query1, function(error, rows, fields){
+        res.render('customers', {data: rows});
+    })
+});
+
+app.post('/add-customer-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let email = parseInt(data['input-email']);
+    if (isNaN(email))
+    {
+        email = 'NULL'
+    }
+
+    let phone = parseInt(data['input-phone']);
+    if (isNaN(phone))
+    {
+        phone = 'NULL'
+    }
+
+    let address = parseInt(data['input-address']);
+    if (isNaN(address))
+    {
+        address = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Customers (firstName, lastName, email, phone, address) VALUES ('${data['input-fname']}', '${data['input-lname']}', ${email}, ${phone}, ${address})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/customers');
+        }
+    })
+});
+
+app.delete('/delete-customer-ajax', function(req,res,next){
+    let data = req.body;
+    let customerID = parseInt(data.id);
+    let delete_customer = `DELETE FROM Customers WHERE customerID = ?`;
+  
+  
+          // Run the 1st query
+            db.pool.query(delete_customer, [customerID], function(error, rows, fields){
+            if (error) {
+
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error);
+                res.sendStatus(400);
+            }
+            else {
+                res.sendStatus(204);
+        }
+  })
+});
+
+app.get('/employees', function(req, res)
+{
+    let query1 = `SELECT * FROM Employees;`
+    db.pool.query(query1, function(error, rows, fields){
+        res.render('employees', {data: rows});
+    })
+});
+app.post('/add-employee-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let email = parseInt(data['input-email']);
+    if (isNaN(email))
+    {
+        email = 'NULL'
+    }
+
+    let phone = parseInt(data['input-phone']);
+    if (isNaN(phone))
+    {
+        phone = 'NULL'
+    }
+
+    let address = parseInt(data['input-address']);
+    if (isNaN(address))
+    {
+        address = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Employees (firstName, lastName, email, phone, role) VALUES ('${data['input-fname']}', '${data['input-lname']}', ${email}, ${phone}, '${data['input-role']}' )`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/employees');
+        }
+    })
+});
+
+app.get('/suppliers', function(req, res)
+{
+    let query1 = `SELECT * FROM Suppliers;`
+    db.pool.query(query1, function(error, rows, fields){
+        res.render('suppliers', {data: rows});
+    })
+});
+
+app.post('/add-supplier-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Suppliers (supplierName, location) VALUES ('${data['input-supplierName']}','${data['input-location']}')`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/suppliers');
+        }
+    })
+});
+
+app.get('/products', function(req, res)
+{
+    let query1 = `SELECT * FROM Products;`
+    db.pool.query(query1, function(error, rows, fields){
+        res.render('products', {data: rows});
+    })
+});
+
+app.post('/add-product-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Products (category, name, price, quantity) VALUES ('${data['input-category']}', '${data['input-productName']}', '${data['input-price']}', '${data['input-quantity']}')`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/products');
+        }
+    })
+});
+
+app.delete('/delete-supplier-ajax', function(req,res,next){
+    let data = req.body;
+    let supplierID = parseInt(data.id);
+    let delete_supplier = `DELETE FROM Suppliers WHERE supplierID = ?`;
+  
+  
+          // Run the 1st query
+            db.pool.query(delete_supplier, [supplierID], function(error, rows, fields){
+            if (error) {
+
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error);
+                res.sendStatus(400);
+            }
+            else {
+                res.sendStatus(204);
+                
+        }
+        
+  })
+});
+
+app.delete('/delete-product-ajax', function(req,res,next){
+    let data = req.body;
+    let productID = parseInt(data.id);
+    let delete_product = `DELETE FROM Products WHERE productID = ?`;
+  
+  
+          // Run the 1st query
+            db.pool.query(delete_supplier, [supplierID], function(error, rows, fields){
+            if (error) {
+
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error);
+                res.sendStatus(400);
+            }
+            else {
+                res.sendStatus(204);
+                
+        }
+        
+  })
+});
+
+app.delete('/delete-employee-ajax', function(req,res,next){
+    let data = req.body;
+    let employeeID = parseInt(data.id);
+    let delete_employee = `DELETE FROM Employees WHERE employeeID = ?`;
+  
+  
+          // Run the 1st query
+            db.pool.query(delete_employee, [employeeID], function(error, rows, fields){
+            if (error) {
+
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error);
+                res.sendStatus(400);
+            }
+            else {
+                res.sendStatus(204);
+                
+        }
+        
+  })
+});
+/*
+    LISTENER
+*/
+app.listen(PORT, function(){
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
---
+++ database/DDL.sql
@@ -0,0 +1,126 @@
+-- Employees table
+CREATE TABLE Employees (
+    employeeID INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    phone VARCHAR(12),
+    role VARCHAR(255) NOT NULL
+);
+
+-- Customers table
+CREATE OR REPLACE TABLE Customers (
+    customerID INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    phone VARCHAR(12),
+    address VARCHAR(255)
+);
+
+-- Suppliers table
+CREATE OR REPLACE TABLE Suppliers (
+    supplierID INT AUTO_INCREMENT PRIMARY KEY,
+    supplierName VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL
+);
+
+-- Products table
+CREATE OR REPLACE TABLE Products (
+    productID INT AUTO_INCREMENT PRIMARY KEY,
+    category VARCHAR(255) NOT NULL CHECK (category IN ('Basketball', 'Tennis', 'Golf', 'Soccer', 'Football')),
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT NOT NULL
+);
+
+-- PurchaseOrders table
+CREATE TOR REPLACE TABLE PurchaseOrders (
+    purchaseID INT AUTO_INCREMENT PRIMARY KEY,
+    supplierID INT NOT NULL,
+    purchaseStatus VARCHAR(255) NOT NULL,
+    purchaseDate DATE NOT NULL,
+-- keep purchase orders when supplier is deleted but update to suppliers should update purchaseorders
+    FOREIGN KEY (supplierID) REFERENCES Suppliers(supplierID) ON UPDATE CASCADE
+);
+
+-- Orders table
+CREATE TABLE Orders (
+    orderID INT AUTO_INCREMENT PRIMARY KEY,
+    customerID INT NOT NULL,
+    -- initially set employee to NULL as optional
+    employeeID INT NULL,
+    orderType VARCHAR(255) NOT NULL,
+    orderStatus VARCHAR(255) NOT NULL,
+    orderDate DATE NOT NULL,
+    total DECIMAL(10, 2) NOT NULL,
+-- Keep previous orders for deleted customers, since employee is optional no need to cascade
+    FOREIGN KEY (customerID) REFERENCES Customers(customerID) ON UPDATE CASCADE,
+    FOREIGN KEY (employeeID) REFERENCES Employees(employeeID) ON UPDATE CASCADE 
+);
+
+-- Junction table for Orders and Products (OrderProducts)
+CREATE TABLE OrderProducts (
+    productID INT,
+    orderID INT,
+    quantity INT NOT NULL,
+    priceAtSale INT NOT NULL,
+    PRIMARY KEY (productID, orderID),
+-- keep order productID in the case it is deleted
+    FOREIGN KEY (productID) REFERENCES Products(productID) ON UPDATE CASCADE,
+    FOREIGN KEY (orderID) REFERENCES Orders(orderID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Junction table for Products and PurchaseOrders (ProductsPurchaseOrders)
+CREATE TABLE PurchaseOrderProducts (
+    productID INT,
+    purchaseID INT,
+    quantity INT NOT NULL,
+    unitPrice DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (productID, purchaseID),
+    FOREIGN KEY (productID) REFERENCES Products(productID) ON UPDATE CASCADE,
+    FOREIGN KEY (purchaseID) REFERENCES PurchaseOrders(purchaseID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO Employees (firstName, lastName, email, phone, role) VALUES
+('John', 'Wick', 'johnwick@example.com', 2062062062, 'Sales Associate'),
+('Matt', 'Russo', 'mattrusso@example.com', 2092092092, 'Manager'),
+('Sarah', 'Miller', 'sarahmiller@example.com', 3513513513, 'Sales Associate');
+
+INSERT INTO Customers (firstName, lastName, email, phone, address) VALUES
+('Michael', 'Wilson', 'michaelwilson@example.com', 1234567891, '221 Jane St'),
+('James', 'Turner', 'jamesturner@example.com', 1234567892, '341 River Ave'),
+('Emily', 'Johnson', 'emilyjohnson@example.com', 1234567893, '777 Mission');
+
+INSERT INTO Suppliers (supplierName, location) VALUES
+('Best Sports Goods', 'Chicago'),
+('All Sports Inc', 'Beijing'),
+('Athletics Supplies Ltd.', 'New Jersey');
+
+INSERT INTO Products (productID, category, name, price, quantity) VALUES
+(1, 'Basketball', 'Size 7 Performance Basketball', 59.99, 30),
+(2, 'Tennis', 'Winners Tennis Racket', 119.99, 30),
+(3, 'Golf', 'Golf Gloves', 49.99, 50),
+(4, 'Soccer', 'Indoor Soccer Ball', 36.99, 40),
+(5, 'Football', 'Grit Football Helmet', 79.99, 25);
+
+INSERT INTO Orders (customerID, employeeID, orderType, orderStatus, orderDate, total) VALUES
+(1, 1, 'Store Pickup', 'Completed', '2024-02-01', 120.00),
+(3, 3, 'Retail', 'Completed', '2024-02-03', 300.00),
+(2, 3, 'Online', 'Shipped', '2024-02-05', 239.00);
+
+INSERT INTO OrderProducts (orderID, productID, priceAtSale, quantity) VALUES
+(1, 1, 59.99, 2),
+(2, 4, 39.99, 8),
+(3, 3, 49.99, 5);
+
+INSERT INTO PurchaseOrders (supplierID, purchaseStatus, purchaseDate) VALUES
+(1, 'Processed', '2024-01-25'),
+(2, 'Shipped', '2024-02-03'),
+(3, 'Completed', '2024-02-08');
+
+INSERT INTO PurchaseOrderProducts (purchaseID, productID, unitPrice, quantity) VALUES
+(1, 1, 5.99, 40),
+(1, 3, 19.99, 30),
+(2, 3, 18.59, 60),
+(3, 2, 23.00, 20);
